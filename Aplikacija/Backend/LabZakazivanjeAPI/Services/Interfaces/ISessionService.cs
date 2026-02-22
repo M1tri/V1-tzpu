954c632b3971245491fd1c3dfe21e9202341e2b8
Dimitrije <dimitrije.ilic2004@gmail.com>
2026-02-22 13:40:48 +0100

    Session Service fix

--- Aplikacija/Backend/LabZakazivanjeAPI/Services/Interfaces/ISessionService.cs
+++ Aplikacija/Backend/LabZakazivanjeAPI/Services/Interfaces/ISessionService.cs
@@ -9,6 +9,7 @@ public interface ISessionService
     Task<ServiceResult<IEnumerable<ViewSessionDTO>>> GetSessionsInRoom(int roomId);
     Task<ServiceResult<ViewSessionDTO>> GetSession(int sessionId);
     Task<ServiceResult<ViewSessionDTO>> AddSession(CreateSessionDTO s);
+    Task<ServiceResult<ViewSessionDTO>> EditSession(UpdateSessionDTO s);
     Task<ServiceResult<Dictionary<int, string>>> GetSessionResourceStatus(int sessionId);
     Task<ServiceResult<string>> PromoteAsNext(int sessionId);
     Task<ServiceResult<string>> Activate(int sessionId);
